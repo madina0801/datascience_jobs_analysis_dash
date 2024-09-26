6ff86d1b47a3b866390cdf9abd4ab5819d40f834
Madina <madinatussupova0820@gmail.com>
2024-09-26 13:21:53 +0500

    Industry histogram

--- data-science-jobs-analysis.py
+++ data-science-jobs-analysis.py
@@ -8,13 +8,20 @@ app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(children='Data Science Jobs Analysis', style={'textAlign':'center'}),
-    dcc.Dropdown(df['Job Title'].unique(), 'Data Scientist', id='dropdown-selection'),
-    dcc.Graph(id='salary-distribution-chart')
+    dcc.Dropdown(df['Job Title'].unique(), 'Data Scientist', id='jobtitle-dropdown', style={'margin-bottom': '15px'}),
+    dcc.RadioItems(
+        id='companysize-radiobuttons',
+        options=[
+            {'label': size for size in df['Size'].unique()}
+        ]
+    ),
+    dcc.Graph(id='salary-distribution-chart'),
+    dcc.Graph(id='industry-chart')
 ])
 
 @callback(
     Output('salary-distribution-chart', 'figure'),
-    Input('dropdown-selection', 'value')
+    Input('jobtitle-dropdown', 'value')
 )
 def update_salary_distribution(job_title):
     dff = df[df['Job Title']==job_title]
@@ -22,7 +29,23 @@ def update_salary_distribution(job_title):
         dff,
         y='Salary Estimate',
         title=f'Salary Estimate for {job_title}',
-        labels={'Salary Estimate: Estimated Salary'},
+        labels={'Salary Estimate': 'Estimated Salary'},
+        template='plotly_dark'
+    )
+    return fig
+
+@callback(
+    Output('industry-chart', 'figure'),
+    Input('jobtitle-dropdown', 'value')
+)
+def update_industry_chart(job_title):
+    industry_count = df[df['Job Title']==job_title]['Industry'].value_counts().head(10)
+    fig = px.bar(
+        industry_count,
+        x=industry_count.index,
+        y=industry_count.values,
+        title=f'Top 10 industries hiring for {job_title}',
+        labels={'x': 'Industry', 'y': 'Number of jobs'},
         template='plotly_dark'
     )
     return fig
